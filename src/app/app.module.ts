622b857b9a760705c30e1b93e7d4fc30dbca5973
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-06-01 22:39:46 -0300

    Curso Angular 9 #04 Inserindo Componentes Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 // Módulo raiz da aplicação!
 // Aqui contém todas as declarações e imports da aplicação!
@@ -18,7 +22,10 @@ import { HomeComponent } from './views/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
