08916c62dbf6388e3306d9782a1ef941aa7939d7
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-06-01 22:20:08 -0300

    Curso Angular 9 #03 Criando o primeiro Componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-
-const routes: Routes = [];
+/** 
+ * Aqui que vamos mapear, mostrar ao Angular, dependendo do caminho(path) qual 
+ * é o componente que ele dever seguir, abrir!
+ * Após a criação de um componente, é necessário criar o roteamento para componente.
+ */
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
 
+// Módulo raiz da aplicação!
+// Aqui contém todas as declarações e imports da aplicação!
+// Todo componente que for criado, tem que ser declarado neste arquivo!
+// Será um arquivo que estará sempre sendo editado!
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
