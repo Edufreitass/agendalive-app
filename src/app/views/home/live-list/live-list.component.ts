b5c88be9d8b3e29bf7af2111e4572ef98bc8223e
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-06-25 12:29:52 -0300

    Curso Angular 9 #09 Obtendo dados da API externa (back-end)

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LiveService } from 'src/app/shared/service/live.service';
+import { Live } from 'src/app/shared/model/live.model';
 
 @Component({
   selector: 'app-live-list',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  // variavel que vai receber a lista das lives que ja aconteceram
+  livesPrevious: Live[];
+  // obtem a listagem das próximas lives
+  livesNext: Live[];
+
+  constructor(
+    // variavel que inicia a classe LiveService atraves do construtor
+    public liveService: LiveService
+  ) { }
 
   ngOnInit(): void {
+    this.getLives();
+  }
+
+  getLives() {
+    this.liveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+    });
+
+    this.liveService.getLivesWithFlag('next').subscribe(data => {
+      this.livesNext = data.content;
+      console.log(this.livesNext);
+    });
   }
 
 }
